dfb4ef6a630a010ef3f8b0605579504e873bed61
Emerson Suarez <emersonsuarez2904@gmail.com>
2025-01-19 19:35:45 -0500

    feat: :heavy_plus_sign: Agrega Pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: "2025-11-01",
-	devtools: { enabled: true },
-	css: [
-		"vuetify/lib/styles/main.sass",
-		"@mdi/font/css/materialdesignicons.min.css",
+ compatibilityDate: "2025-11-01",
+ devtools: { enabled: true },
+
+ css: [
+					"vuetify/lib/styles/main.sass",
+					"@mdi/font/css/materialdesignicons.min.css",
 	],
-	build: {
-		transpile: ["vuetify"],
+
+ build: {
+					transpile: ["vuetify"],
 	},
+
+ modules: ["@pinia/nuxt"],
 });
